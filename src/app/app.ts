63bd69935169d89cc7343498964dddede9de19bc
Rania Rabie <rania.rabie.cg@gmail.com>
2026-06-05 00:22:46 +0300

    Revert "Merge pull request #1 from realKamel/Landingsec_2"
    This reverts commit 219cac21ce0c33b17f26d3aada44f68a929e377b, reversing
    changes made to bfa74ef14be7883c4557b132f4c7849e16124d31.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HowItWorks } from './Components/how-it-works/how-it-works';
-import { ForWhom } from "./Components/for-whom/for-whom";
-import { Testimonials } from "./Components/testimonials/testimonials";
-import { Cta } from "./Components/cta-section/cta-section";
-import { AppFooter } from "./Components/app-footer/app-footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HowItWorks, ForWhom, Testimonials, Cta, AppFooter],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
