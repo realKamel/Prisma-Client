54af71a3c1427d571c4142ad28a4c564cd28e9dd
omnia shaaban <omniashaaban74@gmail.com>
2026-06-04 23:03:11 +0300

    Landing section 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HowItWorks } from './Components/how-it-works/how-it-works';
+import { ForWhom } from "./Components/for-whom/for-whom";
+import { Testimonials } from "./Components/testimonials/testimonials";
+import { Cta } from "./Components/cta-section/cta-section";
+import { AppFooter } from "./Components/app-footer/app-footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HowItWorks, ForWhom, Testimonials, Cta, AppFooter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
