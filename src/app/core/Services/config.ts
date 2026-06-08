0a8c7c0afad71187090c4002190342ca198ab1f5
Rania Rabie <rania.rabie.cg@gmail.com>
2026-06-08 17:49:35 +0300

    change in platformConfig

--- src/app/core/Services/config.ts
+++ src/app/core/Services/config.ts
@@ -1,14 +1,17 @@
-import { inject, Injectable } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { inject, Injectable, signal } from '@angular/core';
 import { PlatformConfig } from '../Models/platform-config';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ConfigService {
   private http = inject(HttpClient);
 
-  config = toSignal(
-    this.http.get<PlatformConfig>('/assets/config/platform.config.json'),
-    { initialValue: null }
-  );
+  config = signal<PlatformConfig | null>(null);
+
+  load() {
+    return this.http
+      .get<PlatformConfig>('assets/config/platform.config.json')
+      .pipe(tap((data) => this.config.set(data)));
+  }
 }
