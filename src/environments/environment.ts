6e28bd237a0af9b7b61814f6c0a8dd62221bbc27
Rania Rabie <rania.rabie.cg@gmail.com>
2026-06-10 16:08:19 +0300

    edit enviroment files  with teacherId

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:7109/api/v1',
-  teacherId: '467a448a-d15f-45af-85b5-32f87144651e'
+  teacherEmail: 'ahmed@prisma.com'
 };
