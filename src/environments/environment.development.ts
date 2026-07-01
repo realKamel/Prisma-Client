71aed0576ca918b4a3bf9af07dc9cd2cdbe556d3
Mohammed24825 <mfdasd6@gmail.com>
2026-07-01 10:59:16 +0300

    Merge branch 'merge-landing-page' of github.com:realKamel/Prisma-Client into merge-landing-page

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:5117/api/v1',
+//   teacherEmail: 'rania.rabie.cg@gmail.com',
+// };
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5117/api/v1',
-  teacherEmail: 'rania.rabie.cg@gmail.com',
+  teacherEmail: 'ahmed@gmail.com',
 };
+
