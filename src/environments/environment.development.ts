db0406ef3bbf1c792251df7fb7cd3e259d4ecfa5
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-07-12 17:44:34 +0300

    fix api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,5 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:5117/api/v1',
-//   teacherEmail: 'rania.rabie.cg@gmail.com',
-// };
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5117/api/v1',
   teacherEmail: 'ahmed@gmail.com',
 };
-
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,4 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:5117/api/v1',
-//   teacherEmail: 'rania.rabie.cg@gmail.com'
-// };
 export const environment = {
-  apiUrl: 'http://prisma.runasp.net/api/v1',
+  apiUrl: 'https://prisma.runasp.net/api/v1',
   teacherEmail: 'ahmed@gmail.com',
 };
