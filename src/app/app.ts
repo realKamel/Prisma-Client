ab92784b29c3ac2edea305f311985147033f3b3a
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-07-17 18:17:10 +0300

    clean up

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,8 @@ import { Component, inject, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from './core/Services/auth';
 import { toast, NgxSonnerToaster } from 'ngx-sonner';
-import { Cursor } from "./Components/cursor/cursor";
-import { StarsCanvas } from "./Components/stars-canvas/stars-canvas";
-
-// import { CustomToast } from './Components/custom-toast/custom-toast';
+import { Cursor } from './Components/cursor/cursor';
+import { StarsCanvas } from './Components/stars-canvas/stars-canvas';
 
 @Component({
   selector: 'app-root',
@@ -18,21 +16,4 @@ export class App {
   protected readonly toast = toast;
   auth = inject(AuthService);
   router = inject(Router);
-
-  fire() {
-    // toast.custom(CustomToast, {
-    //   componentProps: {
-    //     title: 'This is <br />multiline message',
-    //   },
-    // });
-  }
-  ngOnInit() {
-    // console.log('App Init');
-    // this.auth.login({
-    //   id: '1',
-    //   name: 'Test User',
-    //   email: 'test@test.com',
-    //   role: 'student', // 'student' | 'admin' | 'teacher' | 'assistant'
-    // });
-  }
 }
