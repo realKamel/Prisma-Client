44bcb48ca8213257959f51ab34d1ecd47c3c7f23
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-07-19 20:29:05 +0300

    fixes to app & add translation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
 import {
   ApplicationConfig,
   inject,
+  LOCALE_ID,
   provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import {
   provideRouter,
-  Router,
+  TitleStrategy,
   withComponentInputBinding,
   withInMemoryScrolling,
+  withViewTransitions,
 } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
@@ -18,6 +20,13 @@ import { cookieAuthInterceptor } from './core/interceptors/cookie-auth-intercept
 import { AuthService } from './core/Services/auth';
 import { AccentService } from './core/Services/accent-service';
 import { firstValueFrom } from 'rxjs';
+import { provideTranslateService, TranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideIcons } from '@ng-icons/core';
+import { lucideMessageCircleMore } from '@ng-icons/lucide';
+import { CustomTitleStrategy } from './core/Services/custom-title-strategy';
+
+const initialLang = typeof window !== 'undefined' ? (localStorage.getItem('lang') ?? 'ar') : 'ar';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -29,29 +38,55 @@ export const appConfig: ApplicationConfig = {
         scrollPositionRestoration: 'enabled',
       }),
       withComponentInputBinding(),
+      withViewTransitions(),
     ),
+    {
+      provide: TitleStrategy,
+      useClass: CustomTitleStrategy,
+    },
     provideHttpClient(withInterceptors([errorInterceptorInterceptor, cookieAuthInterceptor])),
     provideAppInitializer(async () => {
       const configService = inject(ConfigService);
       const authService = inject(AuthService);
       const accentService = inject(AccentService);
+      const translateService = inject(TranslateService);
+      var initialLang = localStorage.getItem('lang') ?? 'ar';
+      try {
+        await firstValueFrom(translateService.use(initialLang));
+      } catch (error) {
+        console.error('Failed to load translation core system:', error);
+      }
 
+      // 2. Run your application services setup
       await Promise.all([
         configService.loadAsync().catch((error) => {
-          console.error('فشل تحميل إعدادات المنصة:', error);
+          console.error(translateService.instant('COMMON.ERRORS.CONFIG_LOAD_FAILED'), error);
           return null;
         }),
         authService.loadUserInfoAsync().catch((err) => {
-          console.error('Auth check failed:', err);
-          return null; // guest mode
+          console.error(translateService.instant('COMMON.ERRORS.AUTH_CHECK_FAILED'), err);
+          return null;
         }),
         firstValueFrom(accentService.loadFromServer()).catch((err) => {
-          console.error('فشل تحميل إعدادات اللون:', err);
+          console.error(translateService.instant('COMMON.ERRORS.ACCENT_LOAD_FAILED'), err);
           return null;
         }),
       ]);
 
       return;
     }),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json',
+      }),
+      fallbackLang: 'en',
+      lang: initialLang,
+    }),
+    {
+      provide: LOCALE_ID,
+      useValue: 'ar',
+    },
+    // provideIcons({ lucideMessageCircleMore }),
   ],
 };
---
+++ src/app/core/Services/custom-title-strategy.ts
@@ -0,0 +1,42 @@
+import { Injector, Service, inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+
+@Service()
+export class CustomTitleStrategy extends TitleStrategy {
+  private titleSubscription?: Subscription;
+  private translateService?: TranslateService;
+
+  constructor() {
+    super();
+  }
+
+  private readonly injector = inject(Injector);
+  private readonly titleService = inject(Title);
+
+  override updateTitle(routerState: RouterStateSnapshot): void {
+    const titleKey = this.buildTitle(routerState);
+    this.titleSubscription?.unsubscribe();
+
+    if (titleKey) {
+      // Lazily resolve TranslateService only when a route change occurs
+      if (!this.translateService) {
+        this.translateService = this.injector.get(TranslateService);
+      }
+
+      this.titleSubscription = this.translateService
+        .stream(titleKey)
+        .subscribe((translatedTitle: string) => {
+          this.titleService.setTitle(translatedTitle);
+        });
+    } else {
+      this.titleService.setTitle('Prisma');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.titleSubscription?.unsubscribe();
+  }
+}
