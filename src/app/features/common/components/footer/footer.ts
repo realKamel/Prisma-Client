0eaea5eadd37f83ce193c40c64c78871fa7a0fd0
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-08-28 04:03:27 +0300

    refactor(footer): use currentYear signal and update component name

--- src/app/features/common/components/footer/footer.ts
+++ src/app/features/common/components/footer/footer.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class FooterComponent {
+  protected readonly currentYear = signal(new Date().getFullYear());
+}
--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -5,13 +5,13 @@ import { AuthStore } from '../../core/stores/auth.store';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideFileExclamationPoint } from '@ng-icons/lucide';
 import { NavbarComponent } from '../../features/common/components/navbar/navbar';
-import { Footer } from '../../features/common/components/footer/footer';
+import { FooterComponent } from '../../features/common/components/footer/footer';
 // import { AiChatComponent } from '../../features/student/components/ai-chat-component/ai-chat-component';
 import { StarsCanvas } from '../../features/common/components/stars-canvas/stars-canvas';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, StarsCanvas, NavbarComponent, Footer, NgIcon],
+  imports: [RouterOutlet, StarsCanvas, NavbarComponent, FooterComponent, NgIcon],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
   viewProviders: [provideIcons({ lucideFileExclamationPoint })],
