0658a665785bc502c6fda7ea0d08f3f52f62e147
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-09-13 03:37:47 +0300

    feat(animations): add motion animations to auth buttons

--- src/app/features/common/components/navbar/components/auth-buttons/auth-buttons.ts
+++ src/app/features/common/components/navbar/components/auth-buttons/auth-buttons.ts
@@ -1,10 +1,11 @@
 import { Component, model } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
+import { NgmMotionDirective } from '@scripttype/ng-motion';
 
 @Component({
   selector: 'app-auth-buttons',
-  imports: [RouterLink, TranslatePipe],
+  imports: [RouterLink, TranslatePipe, NgmMotionDirective],
   templateUrl: './auth-buttons.html',
   styleUrl: './auth-buttons.css',
 })
